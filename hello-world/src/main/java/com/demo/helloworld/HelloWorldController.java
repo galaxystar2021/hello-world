c046cabbdaad78f1a0f4869275224cc14800355b
Yu Huang <starbing23@gmail.com>
2021-08-22 10:53:10 -0700

    Source tree is easier to review locally

--- hello-world/src/main/java/com/demo/helloworld/HelloWorldController.java
+++ hello-world/src/main/java/com/demo/helloworld/HelloWorldController.java
@@ -13,6 +13,6 @@ public class HelloWorldController {
 
     @RequestMapping("/more")
     public String helloWorld2() {
-        return "Hello World from Turbo House, more stories bin wu...";
+        return "Hello World from Turbo House, more stories bin wu 2...";
     }
 }
