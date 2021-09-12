d5fbdbe7d4afe7747fa008368697c23d1dfe7ebd
Yu Huang <starbing23@gmail.com>
2021-09-12 09:40:35 -0700

    change app file

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,72 +1,19 @@
-'use strict';
+import React, { Component } from 'react';
+import { render as _render } from 'react-dom';
 
-// tag::vars[]
-import { Component } from 'react'; // <1>
-import { render as _render } from 'react-dom'; // <2>
-// const client = require('./client'); // <3>
-// end::vars[]
+class App extends Component {
+  componentDidMount() {
+  }
 
-// tag::app[]
-class App extends Component { // <1>
-
-	constructor(props) {
-		super(props);
-		this.state = {employees: []};
-	}
-
-	componentDidMount() { // <2>
-		// client({method: 'GET', path: '/api/employees'}).done(response => {
-		// 	this.setState({employees: response.entity._embedded.employees});
-		// });
-	}
-
-	render() { // <3>
-		return (
-			<div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
-		)
-	}
+  render() { // <3>
+    return (
+      <div>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+    );
+  }
 }
-// end::app[]
 
-// // tag::employee-list[]
-// class EmployeeList extends React.Component{
-// 	render() {
-// 		const employees = this.props.employees.map(employee =>
-// 			<Employee key={employee._links.self.href} employee={employee}/>
-// 		);
-// 		return (
-// 			<table>
-// 				<tbody>
-// 					<tr>
-// 						<th>First Name</th>
-// 						<th>Last Name</th>
-// 						<th>Description</th>
-// 					</tr>
-// 					{employees}
-// 				</tbody>
-// 			</table>
-// 		)
-// 	}
-// }
-// // end::employee-list[]
-
-// // tag::employee[]
-// class Employee extends React.Component{
-// 	render() {
-// 		return (
-// 			<tr>
-// 				<td>{this.props.employee.firstName}</td>
-// 				<td>{this.props.employee.lastName}</td>
-// 				<td>{this.props.employee.description}</td>
-// 			</tr>
-// 		)
-// 	}
-// }
-// // end::employee[]
-
-// tag::render[]
 _render(
-	<App />,
-	document.getElementById('react')
-)
+  <App />,
+  document.getElementById('react'),
+);
 // end::render[]
